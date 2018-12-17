53229387a46b133944ae460ba586f2651e2964aa
zhoulk <849693228@qq.com>
2018-12-17 20:59:46 +0800

    add login.proto

--- PixelFarm/Assets/LuaFramework/Lua/PixelFarm/Login/Logic/LoginLogic.lua
+++ PixelFarm/Assets/LuaFramework/Lua/PixelFarm/Login/Logic/LoginLogic.lua
@@ -1,3 +1,5 @@
+require "3rd.pblua.login_pb"
+
 local _LoginLogic = class()
 
 -- 登录
@@ -6,28 +8,26 @@ function _LoginLogic:Login(accout, password, cb)
 
     LocalDataManager:SaveUid(accout)
 
-    local login = login_pb.LoginRequest();
-    login.id = 2000;
-    login.name = accout;
-    login.email = 'jarjin@163.com';
+    local login = login_pb.LoginRequest()
+    login.account = accout
+    login.password = password
     
-    local msg = login:SerializeToString();
+    local msg = login:SerializeToString()
     print("login msg = " .. msg)
 
 
-    Event.AddListener("1", function(buffer)
+    Event.AddListener("101", function(buffer)
         print(buffer)
-        local data = buffer:ReadBuffer();
+        local data = buffer:ReadBuffer()
 
-        local msg = login_pb.LoginResponse();
-        msg:ParseFromString(data);
-        print(' msg:>'..msg.id);
-    end); 
-    local buffer = ByteBuffer.New();
-    buffer:WriteShort(0);
-    -- buffer:WriteByte(ProtocalType.PB_LUA);
-    buffer:WriteBuffer(msg);
-    networkMgr:SendMessage(buffer);
+        local msg = login_pb.LoginResponse()
+        msg:ParseFromString(data)
+        print(' msg:>'..msg.uid)
+    end) 
+    local buffer = ByteBuffer.New()
+    buffer:WriteShort(100)
+    buffer:WriteBuffer(msg)
+    networkMgr:SendMessage(buffer)
 
     if cb then
         cb()
