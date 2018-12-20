ab84a250749f03437769f2aef21809795feb1526
zhoulk <849693228@qq.com>
2018-12-20 22:07:53 +0800

    1

--- PixelFarm/Assets/LuaFramework/Lua/PixelFarm/Manager/LocalDataManager.lua
+++ PixelFarm/Assets/LuaFramework/Lua/PixelFarm/Manager/LocalDataManager.lua
@@ -1,23 +1,36 @@
-local _LocalDataManager = class()
-LocalDataManager = _LocalDataManager.Instance()
+local _M = class()
+LocalDataManager = _M.Instance()
 
-LocalDataKey = {
-    PLAYER_UID = "player_uid"
-}
+-- LocalDataKey = {
+--     PLAYER_UID = "player_uid"
+-- }
 
--- 获取本地用户ID
-function _LocalDataManager:GetUid()
-    if PlayerPrefs.HasKey(LocalDataKey.PLAYER_UID) then
-        return PlayerPrefs.GetString(LocalDataKey.PLAYER_UID)
-    else
-        return ""
-    end
+-- -- 获取本地用户ID
+-- function _M:GetUid()
+--     if PlayerPrefs.HasKey(LocalDataKey.PLAYER_UID) then
+--         return PlayerPrefs.GetString(LocalDataKey.PLAYER_UID)
+--     else
+--         return ""
+--     end
+-- end
+
+-- function _M:SaveUid(uid)
+--     if uid and #uid > 0 then
+--         PlayerPrefs.SetString(LocalDataKey.PLAYER_UID, uid)
+--     else
+--         print("[LocalDataManager] SaveUid error!  uid = " .. uid)
+--     end
+-- end
+
+function _M:Save(key, data)
+    local str = tabStr(data)
+    print("[LocalDataManager.Save] str = " .. str)
+
+    PlayerPrefs.SetString(key, str)
 end
 
-function _LocalDataManager:SaveUid(uid)
-    if uid and #uid > 0 then
-        PlayerPrefs.SetString(LocalDataKey.PLAYER_UID, uid)
-    else
-        print("[LocalDataManager] SaveUid error!  uid = " .. uid)
-    end
+function _M:Load(key)
+    local str = PlayerPrefs.GetString(key)
+    print("[LocalDataManager.Load] str = " .. str)
+    return strTab(str)
 end
--- PixelFarm/Assets/LuaFramework/Lua/PixelFarm/Modules/Login/Logic/LoginLogic.lua
+++ PixelFarm/Assets/LuaFramework/Lua/PixelFarm/Modules/Login/Logic/LoginLogic.lua
@@ -5,21 +5,19 @@ local _LoginLogic = class()
 -- 登录
 -- cb(succeed, err)
 function _LoginLogic:Login(accout, password, cb)
-    print("[LoginLogic] Login account = " .. accout .. " password = " .. password)
+    print("[LoginLogic.Login] account = " .. accout .. " password = " .. password)
 
     local loginResponseFunc = function(buffer)
         local data = buffer:ReadBuffer()
 
-        print("[LoginLogic] Login response")
+        print("[LoginLogic.Login] response")
 
         local decode = protobuf.decode("msg.LoginResponse", data)
 
-        print("[LoginLogic] Login response = " .. tabStr(decode))
-        print(decode.code)
-        print(login_pb.SUCCESS)
+        print("[LoginLogic.Login] response = " .. tabStr(decode))
 
         if decode.code == "SUCCESS" then
-            LocalDataManager:SaveUid(decode.uid)
+            self:SaveUid(decode.uid)
             if cb then
                 cb(true)
             end
@@ -50,17 +48,17 @@ function _LoginLogic:Registe(accout, password, cb)
     local registeResponseFunc = function(buffer)
         local data = buffer:ReadBuffer()
 
-        print("[LoginLogic] Registe response")
+        print("[LoginLogic.Registe] response")
 
         local decode = protobuf.decode("msg.RegisteResponse", data)
 
-        print("[LoginLogic] Registe response = " .. tabStr(decode) .. type(decode))
+        print("[LoginLogic.Registe] response = " .. tabStr(decode) .. type(decode))
         print(decode.code)
         print(decode.err.code)
         print(decode.err.msg)
         
         if decode.code == "SUCCESS" then
-            LocalDataManager:SaveUid(decode.uid)
+            self:SaveUid(decode.uid)
             if cb then
                 cb(true)
             end
@@ -83,6 +81,18 @@ function _LoginLogic:Registe(accout, password, cb)
     buffer:WriteShort(Protocal.KeyOf("RegisteRequest"))
     buffer:WriteBuffer(code)
     networkMgr:SendMessage(buffer)
+end
+
+function _LoginLogic:SaveUid(uid)
+    local _UidO = {}
+    _UidO.uid = uid
+    LocalDataManager:Save(MoudleNames.Login .. "Uid", _UidO)
+end
+
+function _LoginLogic:LoadUid()
+    local key = MoudleNames.Login .. "Uid"
+    local _UidO = LocalDataManager:Load(key)
+    return _UidO.uid
 end
 
 return _LoginLogic
